929a383228c9ce2110e1f06946f76117f924b70c
Davion-cook <Davion.cook13@gmail.com>
2026-03-11 19:57:49 -0500

    Add comment for byte_list initialization
    Added a comment to clarify the purpose of the byte_list.

--- functions/text_convert.py
+++ functions/text_convert.py
@@ -2,7 +2,8 @@ def text_to_bytes(text):
     # AES needs exactly 16 characters (16 bytes)
     if len(text) != 16:
         raise ValueError("Message must be exactly 16 characters")
-
+    
+    # create an arry to store characters from string
     byte_list = []
 
     # Convert each character to its UTF-8 value
