7d876f719eed1a66b001554df62b211f8f0fb6ff
Davion-cook <Davion.cook13@gmail.com>
2026-03-12 08:10:48 +0000

    new test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions import text_convert
 from functions import bytes_to_state
+from functions import state_to_hexa
 
 message = "HELLO_AES_123456"
 bytes_data = text_convert.text_to_bytes(message)
@@ -12,3 +13,7 @@ state = bytes_to_state.bytes_to_state(bytes_data)
 for row in state:
     print(row)
 
+hex_state = state_to_hexa.state_to_hex(state)
+
+for row in hex_state:
+    print(row)
