49555a2df05ac0eb5d97d8a1d7fa203a72903964
Davion-cook <Davion.cook13@gmail.com>
2026-03-12 02:56:31 +0000

    revised double loop

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from functions import text_convert
+from functions import bytes_to_state
 
 message = "HELLO_AES_123456"
 bytes_data = text_convert.text_to_bytes(message)
 
 print(bytes_data)
+
+
+state = bytes_to_state.bytes_to_state(bytes_data)
+
+for row in state:
+    print(row)
 
